4a642386936e18dc1a74d1f78efa4c43f66d9d15
Sandeep Nair <sandeepnair@Sandeeps-MacBook-Air.local>
2022-06-20 20:38:56 +0530

    created movie list, only with names

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,8 @@ export { default as Movies } from './Movies/Movies.jsx';
 export { default as NavBar } from './NavBar/NavBar.jsx';
 export { default as Profile } from './Profile/Profile.jsx';
 export { default as Sidebar } from './Sidebar/Sidebar.jsx';
+export { default as MovieList } from './MovieList/MovieList.jsx';
+export { default as Movie } from './Movie/Movie.jsx';
+
 
 
