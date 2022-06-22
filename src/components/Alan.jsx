e65d32d0b5b3a16e176df08a5ae582b11d4cdf8d
Sandeep Nair <sandeepnair@Sandeeps-MacBook-Air.local>
2022-06-22 11:56:14 +0530

    second commit with alan

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import { ColorModeContext } from './utils/ToggleColorMode';
+import { fetchToken } from './utils';
 
 const useAlan = () => {
 
@@ -11,12 +12,17 @@ const useAlan = () => {
         alanBtn({
             key: 'b2c7a92bcfb500230254ce05f8a2635e2e956eca572e1d8b807a3e2338fdd0dc/stage',
             onCommand: ({command, mode}) => {
-              if (command === 'changeMode') {
+              if (command === 'changeMode') {   
                   if(mode === 'light'){
                     setMode('light')
                   } else {
                     setMode('dark')
                   }
+              } else if(command === 'login'){
+                    fetchToken()
+              } else if(command === 'logout'){
+                  localStorage.clear()
+                  window.location.href = '/'   
               }
             }
         });
