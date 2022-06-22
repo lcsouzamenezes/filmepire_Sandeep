a96acdfe555ffe219c5b0da0538084af930f552c
Sandeep Nair <sandeepnair@Sandeeps-MacBook-Air.local>
2022-06-22 13:04:03 +0530

    featured card

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB';
-import { MovieList, Pagination  } from '..';
-import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import { MovieList, Pagination, FeaturedMovie  } from '..';
 
 
 const Movies = () => {
@@ -11,7 +10,7 @@ const Movies = () => {
   const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory )
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
   const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'))
-  const numberOfMovies = lg ? 16 : 18
+  const numberOfMovies = lg ? 17 : 19
 
   if(isFetching){
     return (
@@ -37,7 +36,8 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies={data} numberOfMovies={numberOfMovies} />
+      <FeaturedMovie movie={data.results[0]} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages} />  
     </div>
   )
