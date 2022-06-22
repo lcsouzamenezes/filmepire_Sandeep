b9d897a8d47c4f46a9a088d3ae8c063b270f79d8
Sandeep Nair <sandeepnair@Sandeeps-MacBook-Air.local>
2022-06-22 11:44:57 +0530

    first alan implementation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 import useStyles from './styles'
 import { Actors, MovieInformation, Movies, NavBar, Profile } from './'
-
+import useAlan from './Alan';
 
 const App = () => {
 
     const classes = useStyles()
+    const alanBtnContainer = useRef()
+
+    useAlan()
 
     return (
         <div className={classes.root}>
@@ -30,6 +33,7 @@ const App = () => {
             </Route>
         </Switch>
     </main>
+    <div ref={alanBtnContainer} />
   </div>
     )
 };
